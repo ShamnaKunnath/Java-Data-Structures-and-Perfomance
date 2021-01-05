0480d449ebb697c4a4cb461915f46fc671ea46bb
shamnak <shamna.k@oracle.com>
2021-01-05 23:39:19 +0530

    Implement the getFleschScore method in Document.java

--- src/document/Document.java
+++ src/document/Document.java
@@ -1,6 +1,6 @@
 package document;
 
-/**
+/** 
  * A class that represents a text document
  * @author UC San Diego Intermediate Programming MOOC team
  */
@@ -12,7 +12,7 @@ import java.util.regex.Pattern;
 public abstract class Document {
 
 	private String text;
-
+	
 	/** Create a new document from the given text.
 	 * Because this class is abstract, this is used only from subclasses.
 	 * @param text The text of the document.
@@ -21,12 +21,12 @@ public abstract class Document {
 	{
 		this.text = text;
 	}
-
-	/** Returns the tokens that match the regex pattern from the document
+	
+	/** Returns the tokens that match the regex pattern from the document 
 	 * text string.
-	 * @param pattern A regular expression string specifying the
+	 * @param pattern A regular expression string specifying the 
 	 *   token pattern desired
-	 * @return A List of tokens from the document text that match the regex
+	 * @return A List of tokens from the document text that match the regex 
 	 *   pattern
 	 */
 	protected List<String> getTokens(String pattern)
@@ -34,44 +34,72 @@ public abstract class Document {
 		ArrayList<String> tokens = new ArrayList<String>();
 		Pattern tokSplitter = Pattern.compile(pattern);
 		Matcher m = tokSplitter.matcher(text);
-
+		
 		while (m.find()) {
 			tokens.add(m.group());
 		}
-
+		
 		return tokens;
 	}
-
+	
 	/** This is a helper function that returns the number of syllables
-	 * in a word.  You should write this and use it in your
+	 * in a word.  You should write this and use it in your 
 	 * BasicDocument class.
-	 *
-	 * You will probably NOT need to add a countWords or a countSentences
-	 * method here.  The reason we put countSyllables here because we'll
+	 * 
+	 * You will probably NOT need to add a countWords or a countSentences 
+	 * method here.  The reason we put countSyllables here because we'll 
 	 * use it again next week when we implement the EfficientDocument class.
-	 *
-	 * For reasons of efficiency you should not create Matcher or Pattern
-	 * objects inside this method. Just use a loop to loop through the
-	 * characters in the string and write your own logic for counting
+	 * 
+	 * For reasons of efficiency you should not create Matcher or Pattern 
+	 * objects inside this method. Just use a loop to loop through the 
+	 * characters in the string and write your own logic for counting 
 	 * syllables.
-	 *
+	 * 
 	 * @param word  The word to count the syllables in
-	 * @return The number of syllables in the given word, according to
-	 * this rule: Each contiguous sequence of one or more vowels is a syllable,
-	 *       with the following exception: a lone "e" at the end of a word
-	 *       is not considered a syllable unless the word has no other syllables.
+	 * @return The number of syllables in the given word, according to 
+	 * this rule: Each contiguous sequence of one or more vowels is a syllable, 
+	 *       with the following exception: a lone "e" at the end of a word 
+	 *       is not considered a syllable unless the word has no other syllables. 
 	 *       You should consider y a vowel.
 	 */
 	protected int countSyllables(String word)
 	{
-		// TODO: Implement this method so that you can call it from the
-	    // getNumSyllables method in BasicDocument (module 2) and
+		// TODO: Implement this method so that you can call it from the 
+	    // getNumSyllables method in BasicDocument (module 2) and 
 	    // EfficientDocument (module 3).
-	    return 0;
+		int c=0;
+		int i=0,flag=0;
+		while(i<word.length()) {
+			if(i==word.length()-1&&c!=0&&(word.charAt(i)=='e' ||word.charAt(i)=='E')) {
+				i++;
+			}
+			while(i<word.length() && 
+				   (word.charAt(i) =='a'||
+					word.charAt(i)=='e'||
+					word.charAt(i)=='i'||
+					word.charAt(i)=='o'||
+					word.charAt(i)=='u'||
+					word.charAt(i)=='y'||
+					word.charAt(i) =='A'||
+					word.charAt(i)=='E'||
+					word.charAt(i)=='I'||
+					word.charAt(i)=='O'||
+					word.charAt(i)=='U'||
+					word.charAt(i)=='Y')) {
+				flag=1;
+				i++;
+			}
+			if(flag==1) {
+				c++;
+				flag=0;
+			}
+			i++;	
+		}
+	    return c;
 	}
-
+	
 	/** A method for testing
-	 *
+	 * 
 	 * @param doc The Document object to test
 	 * @param syllables The expected number of syllables
 	 * @param words The expected number of words
@@ -87,21 +115,21 @@ public abstract class Document {
 		int wordsFound = doc.getNumWords();
 		int sentFound = doc.getNumSentences();
 		if (syllFound != syllables) {
-			System.out.println("\nIncorrect number of syllables.  Found " + syllFound
+			System.out.println("\nIncorrect number of syllables.  Found " + syllFound 
 					+ ", expected " + syllables);
 			passed = false;
 		}
 		if (wordsFound != words) {
-			System.out.println("\nIncorrect number of words.  Found " + wordsFound
+			System.out.println("\nIncorrect number of words.  Found " + wordsFound 
 					+ ", expected " + words);
 			passed = false;
 		}
 		if (sentFound != sentences) {
-			System.out.println("\nIncorrect number of sentences.  Found " + sentFound
+			System.out.println("\nIncorrect number of sentences.  Found " + sentFound 
 					+ ", expected " + sentences);
 			passed = false;
 		}
-
+		
 		if (passed) {
 			System.out.println("passed.\n");
 		}
@@ -110,31 +138,34 @@ public abstract class Document {
 		}
 		return passed;
 	}
-
-
+	
+	
 	/** Return the number of words in this document */
 	public abstract int getNumWords();
-
+	
 	/** Return the number of sentences in this document */
 	public abstract int getNumSentences();
-
+	
 	/** Return the number of syllables in this document */
 	public abstract int getNumSyllables();
-
+	
 	/** Return the entire text of this document */
 	public String getText()
 	{
 		return this.text;
 	}
-
+	
 	/** return the Flesch readability score of this document */
 	public double getFleschScore()
 	{
-	    // TODO: You will play with this method in week 1, and
+	    // TODO: You will play with this method in week 1, and 
 		// then implement it in week 2
-	    return text.length();
+		double words=getNumWords();
+		double sentences=getNumSentences();
+		double syllables=getNumSyllables();
+		return 206.835-1.015*(words/sentences)-84.6*(syllables/words);
 	}
-
-
-
+	
+	
+	
 }
