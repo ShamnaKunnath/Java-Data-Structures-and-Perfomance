4b040152a314484144d124f4ea4b64c34b6f2419
shamnak <shamnakunnath54@gmail.com>
2021-12-03 19:40:02 +0530

    Week4:Markov Text Generation-Step1

--- src/textgen/MarkovTextGeneratorLoL.java
+++ src/textgen/MarkovTextGeneratorLoL.java
@@ -5,37 +5,59 @@ import java.util.List;
 import java.util.ListIterator;
 import java.util.Random;
 
-/** 
+/**
  * An implementation of the MTG interface that uses a list of lists.
- * @author UC San Diego Intermediate Programming MOOC team 
+ * @author UC San Diego Intermediate Programming MOOC team
  */
 public class MarkovTextGeneratorLoL implements MarkovTextGenerator {
 
 	// The list of words with their next words
-	private List<ListNode> wordList; 
-	
+	private List<ListNode> wordList;
+
 	// The starting "word"
 	private String starter;
-	
+
 	// The random number generator
 	private Random rnGenerator;
-	
+
 	public MarkovTextGeneratorLoL(Random generator)
 	{
 		wordList = new LinkedList<ListNode>();
 		starter = "";
 		rnGenerator = generator;
 	}
-	
-	
+
+
 	/** Train the generator by adding the sourceText */
 	@Override
 	public void train(String sourceText)
 	{
 		// TODO: Implement this method
+		String[] words=sourceText.split(" ");
+		starter= words[0];
+		String prevWord=starter;
+		int flag=0;
+		for(int i=1;i<words.length;i++) {
+			for(ListNode node:wordList) {
+				if(node.getWord().equals(prevWord)) {
+					node.addNextWord(words[i]);
+					flag=1;
+				}
+			}
+			if(flag==0) {
+				ListNode node= new ListNode(prevWord);
+				node.addNextWord(words[i]);
+				wordList.add(node);
+			}
+			prevWord=words[i];
+			flag=0;
+		}
+		ListNode node= new ListNode(prevWord);
+		node.addNextWord(starter);
+		wordList.add(node);
 	}
-	
-	/** 
+
+	/**
 	 * Generate the number of words requested.
 	 */
 	@Override
@@ -43,8 +65,8 @@ public class MarkovTextGeneratorLoL implements MarkovTextGenerator {
 	    // TODO: Implement this method
 		return null;
 	}
-	
-	
+
+
 	// Can be helpful for debugging
 	@Override
 	public String toString()
@@ -56,20 +78,20 @@ public class MarkovTextGeneratorLoL implements MarkovTextGenerator {
 		}
 		return toReturn;
 	}
-	
+
 	/** Retrain the generator from scratch on the source text */
 	@Override
 	public void retrain(String sourceText)
 	{
 		// TODO: Implement this method.
 	}
-	
+
 	// TODO: Add any private helper methods you need here.
-	
-	
+
+
 	/**
 	 * This is a minimal set of tests.  Note that it can be difficult
-	 * to test methods/classes with randomized behavior.   
+	 * to test methods/classes with randomized behavior.
 	 * @param args
 	 */
 	public static void main(String[] args)
@@ -77,7 +99,9 @@ public class MarkovTextGeneratorLoL implements MarkovTextGenerator {
 		// feed the generator a fixed random value for repeatable behavior
 		MarkovTextGeneratorLoL gen = new MarkovTextGeneratorLoL(new Random(42));
 		String textString = "Hello.  Hello there.  This is a test.  Hello there.  Hello Bob.  Test again.";
+		gen.train("hi there hi Leo");
 		System.out.println(textString);
+		System.out.println(gen.toString());
 		gen.train(textString);
 		System.out.println(gen);
 		System.out.println(gen.generateText(20));
@@ -113,22 +137,22 @@ public class MarkovTextGeneratorLoL implements MarkovTextGenerator {
 
 }
 
-/** Links a word to the next words in the list 
+/** Links a word to the next words in the list
  * You should use this class in your implementation. */
 class ListNode
 {
     // The word that is linking to the next words
 	private String word;
-	
+
 	// The next words that could follow it
 	private List<String> nextWords;
-	
+
 	ListNode(String word)
 	{
 		this.word = word;
 		nextWords = new LinkedList<String>();
 	}
-	
+
 	public String getWord()
 	{
 		return word;
@@ -138,11 +162,11 @@ class ListNode
 	{
 		nextWords.add(nextWord);
 	}
-	
+
 	public String getRandomNextWord(Random generator)
 	{
 		// TODO: Implement this method
-	    // The random number generator should be passed from 
+	    // The random number generator should be passed from
 	    // the MarkovTextGeneratorLoL class
 	    return null;
 	}
@@ -156,7 +180,5 @@ class ListNode
 		toReturn += "\n";
 		return toReturn;
 	}
-	
+
 }
-
-
